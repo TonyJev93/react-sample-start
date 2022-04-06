61c9c0a33861e90d32033f01c15b26e84c1dcc6c
JUN GYEWON <tonyjev93@gmail.com>
2022-04-06 22:24:17 +0900

    feat: 자식 -> 부모 값 전달 구현

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+    state = {
+        userName: '',
+        phone: '',
+    }
+    // e = event 객체
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate(this.state)
+        this.setState({
+            userName: '',
+            phone: '',
+        })
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    name="userName"
+                    placeholder="이름"
+                    onChange={this.handleChange} 
+                    value={this.state.userName} 
+                />
+                <input 
+                    name="phone"
+                    placeholder="전화번호" 
+                    onChange={this.handleChange} 
+                    value={this.state.phone} 
+                />
+                <button type="submit">등록</button>
+                <div>
+                {this.state.userName}  {this.state.phone}
+                </div>
+            </form>
+        );
+    }
+}
+
+export default PhoneForm;
